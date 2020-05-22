c589c2eaf0f1bd39aa0bc95b9916fcdb22079098
Alexander DeRieux <acd1797@vt.edu>
2020-05-22 12:30:23 -0400

    Now imports App component using resolution alias

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 /* Import application component */
-import App from './components/App';
+import App from 'components/App';
 
 /* Render application if container element exists in document */
 const container = document.getElementById('container');
