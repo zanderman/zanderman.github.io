f39201310210295f177916887431249b208a4bdf
Alexander DeRieux <acd1797@vt.edu>
2020-05-22 12:15:42 -0400

    Initial application component

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+/**
+ * Application component
+ **/
+
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render() {
+        return (
+            <p>Hello from App.js!</p>
+        );
+    }
+}
+
+export default App;
